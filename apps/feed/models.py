1e49b115920add3966c0225e9973e6884fa2d16d
csar2g <cesarricardogg@hotmail.com>
2026-04-19 21:39:54 -0700

    Add feed models

--- apps/feed/models.py
+++ apps/feed/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from djago.contrib.auth.models import User
 
 # Create your models here.
+
+Class Publicacion(models.Model):
+    descripcion = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.CharField(max_length=350, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.created_at
+
+Class Reaccion(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    publicacion = models.OneToOneField(Publicacion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.publicacion
+
+Class Comentario(models.Model)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    contenido = models.CharField(max_length=400)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    publicacion = models.OneToOneField(Publicacion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contenido
