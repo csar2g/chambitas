accfde2a590328901b1d85914ad8a7fe47084647
csar2g <cesarricardogg@hotmail.com>
2026-05-16 10:15:12 -0700

    Database settings corrections

--- chambitas/settings.py
+++ chambitas/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'chambitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
