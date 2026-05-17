389a0c49f9a922528921858c1eaddd16d23cb425
VictorMML90 <l22211602@tectijuana.edu.mx>
2026-05-16 22:02:04 -0700

    feat: messaging with images, profile photos and draft conversations

--- apps/message/urls.py
+++ apps/message/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('mensajes/', views.bandeja, name='bandeja'),
     path('mensajes/<int:conversacion_id>/', views.conversacion, name='conversacion'),
+     path('mensajes/iniciar/<int:receptor_id>/', views.iniciar_conversacion, name='iniciar_conversacion'),
+    # Nueva vista para crear conversación desde draft:
+    path('mensajes/nueva/', views.crear_conversacion, name='crear_conversacion'),
 ]
